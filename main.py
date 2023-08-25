3cb8e1fae685d897eb256e94e6fe281ec737830c
lucaslut <143185980+lucaslut@users.noreply.github.com>
2023-08-25 13:59:54 +0200

    Add files via upload
    File organizer

---
+++ main.py
@@ -0,0 +1,50 @@
+# organize a cluttered desktop & hdd by automatically moving it to dated folders (year, month) according to 'date created'
+
+# /Users/lucaslutfiev/Documents/Alle files
+
+import os
+import shutil
+import datetime
+
+def organize_files(source_folder, target_folder):
+    if not os.path.exists(target_folder):
+        os.makedirs(target_folder)
+
+    for root, _, files in os.walk(source_folder):
+        for filename in files:
+            if filename.startswith("._"):
+                continue  # Skip hidden system files
+            source_path = os.path.join(root, filename)
+
+            try:
+                file_creation_time = os.path.getctime(source_path)
+            except FileNotFoundError:
+                continue  # Skip files that can't be accessed
+
+            creation_date = datetime.datetime.fromtimestamp(file_creation_time)
+            target_subfolder = os.path.join(target_folder, str(creation_date.year),
+                                            f"{creation_date.month:02d}")
+
+            if not os.path.exists(target_subfolder):
+                os.makedirs(target_subfolder)
+
+            target_file = os.path.join(target_subfolder, filename)
+            shutil.move(source_path, target_file)
+            print(f"Moved {filename} to {target_subfolder}")
+
+source_folder = "/Volumes/Lucas 1/Source/"  # Update this path
+target_folder = "/Volumes/Lucas 1/Target/"  # Update this path
+
+organize_files(source_folder, target_folder)
+
+
+
+
+
+
+
+
+
+
+
+
